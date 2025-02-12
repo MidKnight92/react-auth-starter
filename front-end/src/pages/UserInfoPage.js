88582811dffebac0bd8b66ac2c5815e671aa4820
Stephanie Viveros <50332616+MidKnight92@users.noreply.github.com>
2025-02-12 19:09:07 +0000

    chore: update dependencies - Updated project dependencies to the latest versions - Replaced deprecated React and React Router code (useHistory -> useNavigate, removed Switch)

--- front-end/src/pages/UserInfoPage.js
+++ front-end/src/pages/UserInfoPage.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export const UserInfoPage = () => {
-    // We'll use the history to navigate the user
+    // We'll use the useNavigate hook to navigate the user
     // programmatically later on (we're not using it yet)
-    const history = useHistory();
+    const navigate = useNavigate();
 
     // These states are bound to the values of the text inputs
     // on the page (see JSX below). 
@@ -42,13 +42,13 @@ export const UserInfoPage = () => {
         // and send them to the "login page"
         alert('Log out functionality not implemented yet');
     }
-    
+
     const resetValues = () => {
         // Reset the text input values to
         // their starting values (the data we loaded from the server)
         alert('Reset functionality not implemented yet');
     }
-    
+
     // And here we have the JSX for our component. It's pretty straightforward
     return (
         <div className="content-container">
