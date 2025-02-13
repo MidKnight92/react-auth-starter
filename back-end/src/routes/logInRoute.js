ead48b036eac3989871b8c5f599eee7916e420b2
Stephanie Viveros <50332616+MidKnight92@users.noreply.github.com>
2025-02-13 15:12:44 +0000

    Add login route

---
+++ back-end/src/routes/logInRoute.js
@@ -0,0 +1,32 @@
+import { getDbConnection } from "../db";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+export const logInRoute = {
+    path: '/api/login',
+    method: 'post',
+    handler: async (req, res) => {
+        const { email: requestEmail, password } = req.body;
+        const db = getDbConnection('react-auth-db');
+        const user = await db.collection('users').findOne({ email: requestEmail });
+        if (!(user && await bcrypt.compare(password, user.passwordHash))) {
+            return res.status(401).json({ message: 'Email or password is incorrect.' });
+        }
+        const { _id: id, isVerified, email, info } = user;
+        const secret = process.env.JWT_TOKEN;
+
+        jwt.sign({ id, isVerified, info, email },
+            secret,
+            {
+                expiresIn: '2d',
+            },
+            (err, token) => {
+                if (err) {
+                    console.error(err);
+                    return res.sendStatus(500);
+                }
+                res.status(200).json({ token });
+            }
+        )
+    }
+}
