49b11bd276394b24d15293879c8ab30abe5ceaa8
Stephanie Viveros <50332616+MidKnight92@users.noreply.github.com>
2025-02-17 01:15:49 +0000

    Refactor loginRoute and signUpRoute to use try catch and remove callback

--- back-end/src/routes/logInRoute.js
+++ back-end/src/routes/logInRoute.js
@@ -6,27 +6,20 @@ export const logInRoute = {
     path: '/api/login',
     method: 'post',
     handler: async (req, res) => {
-        const { email: requestEmail, password } = req.body;
-        const db = getDbConnection('react-auth-db');
-        const user = await db.collection('users').findOne({ email: requestEmail });
-        if (!(user && await bcrypt.compare(password, user.passwordHash))) {
-            return res.status(401).json({ message: 'Email or password is incorrect.' });
-        }
-        const { _id: id, isVerified, email, info } = user;
-        const secret = process.env.JWT_TOKEN;
-
-        jwt.sign({ id, isVerified, info, email },
-            secret,
-            {
-                expiresIn: '2d',
-            },
-            (err, token) => {
-                if (err) {
-                    console.error(err);
-                    return res.status(500).json({ message: 'Error occurred.' });
-                }
-                res.status(200).json({ token });
+        try {
+            const { email: requestEmail, password } = req.body;
+            const db = getDbConnection('react-auth-db');
+            const user = await db.collection('users').findOne({ email: requestEmail });
+            if (!(user && await bcrypt.compare(password, user.passwordHash))) {
+                return res.status(401).json({ message: 'Email or password is incorrect.' });
             }
-        )
+            const { _id: id, isVerified, email, info } = user;
+
+            const token = jwt.sign({ id, isVerified, info, email }, process.env.JWT_TOKEN, { expiresIn: '2d' });
+            res.status(200).json({ token });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'An error occured' });
+        }
     }
 }
--- back-end/src/routes/signUpRoute.js
+++ back-end/src/routes/signUpRoute.js
@@ -6,42 +6,34 @@ export const signUpRoute = {
     path: '/api/signup',
     method: 'post',
     handler: async (req, res) => {
-        const { email, password } = req.body;
-        const db = getDbConnection('react-auth-db');
-        const user = await db.collection('users').findOne({ email });
-        if (user) {
-            return res.status(409).json({ message: 'Conflict: A user with the same email already exists.' });
+        try {
+            const { email, password } = req.body;
+            const db = getDbConnection('react-auth-db');
+            const user = await db.collection('users').findOne({ email });
+            if (user) {
+                return res.status(409).json({ message: 'Conflict: A user with the same email already exists.' });
+            }
+            const passwordHash = await bcrypt.hash(password, 10);
+
+            const documentData = {
+                email,
+                info: {
+                    hairColor: '',
+                    favoriteFood: '',
+                    bio: '',
+                },
+                isVerified: false,
+            };
+            const result = await db.collection('users').insertOne({ ...documentData, passwordHash });
+
+            const { insertedId } = result;
+
+            const token = jwt.sign({ id: insertedId, ...documentData }, process.env.JWT_TOKEN, { expiresIn: '2d' });
+
+            res.status(200).json({ token });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'An error occured' });
         }
-        const passwordHash = await bcrypt.hash(password, 10);
-
-        const documentData = {
-            email,
-            info: {
-                hairColor: '',
-                favoriteFood: '',
-                bio: '',
-            },
-            isVerified: false,
-        };
-        const result = await db.collection('users').insertOne({ ...documentData, passwordHash });
-
-        const { insertedId } = result;
-        const secret = process.env.JWT_TOKEN;
-
-        jwt.sign({
-            id: insertedId,
-            ...documentData,
-        },
-            secret,
-            {
-                expiresIn: '2d',
-            }, (err, token) => {
-                if (err) {
-                    console.error(err)
-                    return res.sendStatus(500);
-                }
-
-                res.status(200).json({ token });
-            })
     }
 }
